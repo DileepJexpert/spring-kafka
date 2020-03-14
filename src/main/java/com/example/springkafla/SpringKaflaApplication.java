84b4e7aa43caf7e4a4d8cffa523d5f3791626eaa
DileepkMaurya <todileepmaurya@gmail.com>
2020-03-14 13:45:44 +0530

    first commit

---
+++ src/main/java/com/example/springkafla/SpringKaflaApplication.java
@@ -0,0 +1,13 @@
+package com.example.springkafla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKaflaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKaflaApplication.class, args);
+    }
+
+}
